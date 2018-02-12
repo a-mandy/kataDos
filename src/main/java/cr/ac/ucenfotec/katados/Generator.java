92bdb86fb5f67d7a2eb900de76e10eaecc65cef1
Amanda Campos Zeledon <acamposz@ucenfotec.ac.cr>
2018-02-11 22:53:50 -0600

    lógica y testing con todos los valores para 4620

--- src/main/java/cr/ac/ucenfotec/katados/Generator.java
+++ src/main/java/cr/ac/ucenfotec/katados/Generator.java
@@ -1,8 +1,28 @@
 package cr.ac.ucenfotec.katados;
 
+import java.util.ArrayList;
+
 public class Generator {
-	public int generate(int number) {
-		return 0;
+	public ArrayList<Integer> generate(int n) {
+		ArrayList<Integer> numeros = new ArrayList<Integer>();
+
+		while (n % 2 == 0) {
+			numeros.add(2);
+			n /= 2;
+		}
+		
+		for(int i = 3; i < Math.sqrt(n);i+=2) {
+			while(n%i == 0) {
+				numeros.add(i);
+				n /= i;
+			}
+		}
+		
+		if(n > 2) {
+			numeros.add(n);
+		}
+
+		return numeros;
 	}
 
 }
--- src/main/java/cr/ac/ucenfotec/katados/generateTest.java
+++ src/main/java/cr/ac/ucenfotec/katados/generateTest.java
@@ -2,6 +2,8 @@ package cr.ac.ucenfotec.katados;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class generateTest {
@@ -9,8 +11,14 @@ public class generateTest {
 	@Test
 	public void test() {
 		Generator test= new Generator();
-		int output= test.generate(2);
-		assertEquals(0,output);
+		
+		ArrayList<Integer> output= test.generate(4620);
+		assertEquals(2,(int)output.get(0));
+		assertEquals(2,(int)output.get(1));
+		assertEquals(3,(int)output.get(2));
+		assertEquals(5,(int)output.get(3));
+		assertEquals(7,(int)output.get(4));
+		assertEquals(11,(int)output.get(5));
 	}
 
 }
