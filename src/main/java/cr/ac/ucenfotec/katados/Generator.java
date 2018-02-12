e4e152ab1145876fc42269b431c18a2126941410
Amanda Campos Zeledon <acamposz@ucenfotec.ac.cr>
2018-02-12 00:18:15 -0600

    refactored method extracted

--- src/main/java/cr/ac/ucenfotec/katados/Generator.java
+++ src/main/java/cr/ac/ucenfotec/katados/Generator.java
@@ -7,29 +7,45 @@ class Generator {
 		ArrayList<Integer> numeros = new ArrayList<Integer>();
 		int[] resul;
 
-		while (n % 2 == 0) {
-			numeros.add(2);
-			n /= 2;
-		}
+		n = porDos(n, numeros);
 		
+		n = porTres(n, numeros);
+
+		if (n > 2) {
+			numeros.add(n);
+		}
+
+		resul = toArray(numeros);
+
+		return resul;
+	}
+
+	private int[] toArray(ArrayList<Integer> numeros) {
+		int[] resul;
+		resul = new int[numeros.size()];
+
+		for (int i = 0; i < numeros.size(); i++) {
+			resul[i] = numeros.get(i);
+		}
+		return resul;
+	}
+
+	private int porTres(int n, ArrayList<Integer> numeros) {
 		for (int i = 3; i <= Math.sqrt(n); i += 2) {
 			while (n % i == 0) {
 				numeros.add(i);
 				n /= i;
 			}
 		}
+		return n;
+	}
 
-		if (n > 2) {
-			numeros.add(n);
+	private int porDos(int n, ArrayList<Integer> numeros) {
+		while (n % 2 == 0) {
+			numeros.add(2);
+			n /= 2;
 		}
-
-		resul = new int[numeros.size()];
-
-		for (int i = 0; i < numeros.size(); i++) {
-			resul[i] = numeros.get(i);
-		}
-
-		return resul;
+		return n;
 	}
 
 }
