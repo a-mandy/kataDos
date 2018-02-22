ec6ce9e0b1e6a076e5ead85d119791c31fca7880
Amanda Campos Zeledon <acamposz@ucenfotec.ac.cr>
2018-02-21 21:09:55 -0600

    primer refactor

--- src/main/java/cr/ac/ucenfotec/katados/ProcesadorRomano.java
+++ src/main/java/cr/ac/ucenfotec/katados/ProcesadorRomano.java
@@ -6,44 +6,49 @@ package cr.ac.ucenfotec.katados;
 
 public class ProcesadorRomano {
 	public int convierte(String x) {
-		int num=0;
+		int resul=0;
 		if(x=="") {
-			num=0;
+			resul=0;
 		}
 		char[] letras = x.toCharArray();
 		int tamanio= letras.length;
-	
+		int num=0;
+		
 		for(int i=0;i<tamanio;i++) {
 			
-			switch(letras[i]) {
-			case 'I':
-				num+=1;
-				break;
-			case 'V':
-				num+=5;
-				break;
-			case 'X':
-				num+=10;
-				break;
-			case 'L':
-				num+=50;
-				break;
-			case 'C':
-				num+=100;
-				break;
-			case 'D':
-				num+=500;
-				break;
-			case 'M':
-				num+=1000;
-				break;
-			}
-			
-			
+			num = toNumber(letras, num, i);
+			resul+=num;
 		}
 		
-		return num;
+		return resul;
 		
+	}
+
+	private int toNumber(char[] letras, int num, int i) {
+		switch(letras[i]) {
+		case 'I':
+			num=1;
+			break;
+		case 'V':
+			num=5;
+			break;
+		case 'X':
+			num=10;
+			break;
+		case 'L':
+			num=50;
+			break;
+		case 'C':
+			num=100;
+			break;
+		case 'D':
+			num=500;
+			break;
+		case 'M':
+			num=1000;
+			break;
+		}
+		return num;
 	}
 
 }
