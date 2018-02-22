574b36233b31443e42895957550973dfb5721d76
Amanda Campos Zeledon <acamposz@ucenfotec.ac.cr>
2018-02-21 19:46:39 -0600

    logica basica para pasar prueba

---
+++ src/main/java/cr/ac/ucenfotec/katados/ProcesadorRomano.java
@@ -0,0 +1,11 @@
+package cr.ac.ucenfotec.katados;
+
+//import org.springframework.boot.SpringApplication;
+
+public class ProcesadorRomano {
+	public int convierte(String x) {
+		return 0;
+		
+	}
+
+}
---
+++ src/test/java/cr/ac/ucenfotec/katados/ProcesadorRomanoTest.java
@@ -0,0 +1,16 @@
+package cr.ac.ucenfotec.katados;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProcesadorRomanoTest {
+
+	@Test
+	public int test1() {
+		int testValue= 0;
+		return testValue;
+		
+	}
+
+}
