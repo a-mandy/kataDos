dd9bc0b2b8a3435c4615eb37501d250afe5a5537
Amanda Campos Zeledon <acamposz@ucenfotec.ac.cr>
2018-02-21 20:55:33 -0600

    Lógica con pasos I yII

--- src/main/java/cr/ac/ucenfotec/katados/ProcesadorRomano.java
+++ src/main/java/cr/ac/ucenfotec/katados/ProcesadorRomano.java
@@ -1,14 +1,48 @@
 package cr.ac.ucenfotec.katados;
 
+//import java.util.ArrayList;
+
 //import org.springframework.boot.SpringApplication;
 
 public class ProcesadorRomano {
 	public int convierte(String x) {
-		int resul=0;
+		int num=0;
 		if(x=="") {
-			resul=0;
+			num=0;
 		}
-		return resul;
+		char[] letras = x.toCharArray();
+		int tamanio= letras.length;
+	
+		for(int i=0;i<tamanio;i++) {
+			
+			switch(letras[i]) {
+			case 'I':
+				num+=1;
+				break;
+			case 'V':
+				num+=5;
+				break;
+			case 'X':
+				num+=10;
+				break;
+			case 'L':
+				num+=50;
+				break;
+			case 'C':
+				num+=100;
+				break;
+			case 'D':
+				num+=500;
+				break;
+			case 'M':
+				num+=1000;
+				break;
+			}
+			
+			
+		}
+		
+		return num;
 		
 	}
 
